b1f3f131261d9d73a3a00257e799b6730cc238e5
rootid <vsinhsawant@gmail.com>
2016-01-01 22:36:28 -0500

    added paper elements

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 
-document.addEventListener('click', function(e) {
-  var pages = document.querySelector('iron-pages');
-  pages.selectNext();
-});
+//document.addEventListener('click', function(e) {
+//  var pages = document.querySelector('iron-pages');
+//  pages.selectNext();
+//});
+
+var title_d = document.querySelector("#app");
+title_d.title = "FriendFlix";
+
+
--- init.sh
+++ init.sh
@@ -3,7 +3,17 @@
 
 #bower install --save Polymer/core-toolbar Polymer/paper-icon-button Polymer/paper-progress
 #bower install --save Polymer/core-elements 
-bower install --save PolymerElements/iron-pages
+#bower install --save PolymerElements/iron-pages
+#bower install --save PolymerElements/iron-list
+
+#bower install --save PolymerElements/paper-toolbar
+#bower install --save PolymerElements/paper-icon-button
+#bower install --save PolymerElements/paper-drawer-panel
+#bower install --save PolymerElements/paper-drawer-panel
+#bower install --save PolymerElements/paper-menu-button
+#bower install --save PolymerElements/paper-header-panel
+
+bower install --save PolymerElements/paper-menu
 
 
-bower install --save PolymerElements/iron-list
+#bower install --save page.js
