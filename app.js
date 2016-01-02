26337d87a3a3845217dea74a26d7fcbcf17ec0c3
rootid <vsinhsawant@gmail.com>
2016-01-01 20:22:54 -0500

    tested iron-pages

---
+++ app.js
@@ -0,0 +1,5 @@
+
+document.addEventListener('click', function(e) {
+  var pages = document.querySelector('iron-pages');
+  pages.selectNext();
+});
--- init.sh
+++ init.sh
@@ -4,3 +4,6 @@
 #bower install --save Polymer/core-toolbar Polymer/paper-icon-button Polymer/paper-progress
 #bower install --save Polymer/core-elements 
 bower install --save PolymerElements/iron-pages
+
+
+bower install --save PolymerElements/iron-list
