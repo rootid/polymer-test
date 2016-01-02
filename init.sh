4216e1b0bbf9ca0ae5d4aafc3ec59edec1a52324
rootid <vsinhsawant@gmail.com>
2016-01-01 19:55:13 -0500

    started to add paper-pages

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 
-bower install --save Polymer/core-toolbar Polymer/paper-icon-button Polymer/paper-progress
+#bower install --save Polymer/core-toolbar Polymer/paper-icon-button Polymer/paper-progress
+#bower install --save Polymer/core-elements 
+bower install --save PolymerElements/iron-pages
